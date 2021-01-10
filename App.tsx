70f19dfba7d43bf06fc2766929136181c63bcc12
dudduss <duddus@berkeley.edu>
2021-01-09 18:41:18 -0800

    some progress, navigation + tabs

--- App.tsx
+++ App.tsx
@@ -1,21 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Home, Profile } from './Screens';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const Stack = createStackNavigator();
+const Tabs = createBottomTabNavigator();
+
+const HomeStack = createStackNavigator();
+const ProfileStack = createStackNavigator();
+
+function HomeStackScreen() {
   return (
-    <View style={styles.container}>
-      <Text>sai ram</Text>
-      <StatusBar style='auto' />
-    </View>
+    <HomeStack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#2AD478',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}
+    >
+      <HomeStack.Screen name='Home' component={Home}></HomeStack.Screen>
+    </HomeStack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function ProfileStackScreen() {
+  return (
+    <ProfileStack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#2AD478',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}
+    >
+      <ProfileStack.Screen
+        name='Profile'
+        component={Profile}
+      ></ProfileStack.Screen>
+    </ProfileStack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tabs.Navigator tabBarOptions={{ activeTintColor: '#2AD478' }}>
+        <Tabs.Screen
+          name='Home'
+          component={HomeStackScreen}
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name='basket' color={color} size={size} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name='Profile'
+          component={ProfileStackScreen}
+          options={{
+            tabBarLabel: 'Profile',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name='face-profile'
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
+      </Tabs.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ Screens.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ScreenContainer } from 'react-native-screens';
+import { Text } from 'react-native';
+
+export function Home() {
+  return (
+    <ScreenContainer>
+      <Text> Home Screen baby </Text>
+    </ScreenContainer>
+  );
+}
+
+export function Profile() {
+  return (
+    <ScreenContainer>
+      <Text> Profile Screen baby </Text>
+    </ScreenContainer>
+  );
+}
