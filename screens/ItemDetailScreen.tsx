eb8e9c963bf5b65aac1537bc6d7674e5426d66f3
dudduss <duddus@berkeley.edu>
2021-01-17 11:17:32 -0800

    some styling

--- screens/ItemDetailScreen.tsx
+++ screens/ItemDetailScreen.tsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { ScreenContainer } from 'react-native-screens';
-import { Text, StyleSheet, View } from 'react-native';
+import { Text, StyleSheet, View, Image } from 'react-native';
 import { RouteProp } from '@react-navigation/native';
 import { HomeStackParamsList } from '../types';
 import { FoodItemDetail } from '../types';
 import { getIpAddress } from '../utils';
 import axios from 'axios';
 import FoodItemsList from '../components/FoodItemsList';
+import StarRating from 'react-native-star-rating';
 
 type ItemDetailScreenRouteProp = RouteProp<HomeStackParamsList, 'ItemDetail'>;
 
@@ -15,7 +16,7 @@ type ItemDetailScreenProps = {
 };
 
 type ItemDetailScreenState = {
-  itemDetail: FoodItemDetail;
+  itemDetail?: FoodItemDetail;
   isLoading: boolean;
 };
 
@@ -28,38 +29,92 @@ export default class ItemDetailScreen extends Component<
 
     this.state = {
       isLoading: false,
-      itemDetail: [],
+      itemDetail: undefined,
     };
   }
 
-  //   componentDidMount() {
-  //     const { route } = this.props;
-  //     this.setState({ isLoading: true });
-  //     const url =
-  //       'http://' +
-  //       getIpAddress() +
-  //       ':3000/items/search/?searchString=' +
-  //       route.params.searchString;
+  componentDidMount() {
+    const { route } = this.props;
+    this.setState({ isLoading: true });
+    const url =
+      'http://' +
+      getIpAddress() +
+      ':3000/items/search/detail?nix_item_id=' +
+      route.params.item.nix_item_id;
 
-  //     axios
-  //       .get(url)
-  //       .then((response) => {
-  //         this.setState({
-  //           searchedItems: (response.data as unknown) as FoodItem[],
-  //           isLoading: false,
-  //         });
-  //       })
-  //       .catch((error) => console.log(error));
-  //   }
+    axios
+      .get(url)
+      .then((response) => {
+        this.setState({
+          itemDetail: (response.data as unknown) as FoodItemDetail,
+          isLoading: false,
+        });
+      })
+      .catch((error) => console.log(error));
+
+    // We also want to make requests to our "health endpoint" when that is ready + get reviews endpoint
+  }
 
   render() {
     const { route } = this.props;
+    const item = route.params.item;
+
     return (
       <ScreenContainer>
         <View>
-          <Text>{route.params.item.food_name}</Text>
+          <Image source={{ uri: item.image_url }} style={styles.foodImage} />
+          <View style={styles.infoContainer}>
+            <Text style={styles.foodNameText}>{item.food_name}</Text>
+            <Text style={styles.brandNameText}>by {item.brand_name}</Text>
+            <View style={styles.ratingContainer}>
+              <StarRating
+                disabled={true}
+                maxStars={5}
+                rating={item.rating}
+                fullStarColor={'#2AD478'}
+                emptyStarColor={'#2AD478'}
+                starSize={30}
+              ></StarRating>
+              <Text style={styles.numReviewsText}>
+                {item.num_reviews}{' '}
+                {item.num_reviews == 1 ? 'review' : 'reviews'}
+              </Text>
+            </View>
+          </View>
         </View>
       </ScreenContainer>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  foodImage: {
+    textAlign: 'center',
+    marginTop: 20,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    width: '40%',
+    paddingTop: '40%',
+  },
+  foodNameText: {
+    fontSize: 20,
+    fontWeight: '400',
+  },
+  brandNameText: {
+    marginTop: 5,
+    fontSize: 12,
+    fontWeight: '300',
+  },
+  infoContainer: {
+    marginTop: 20,
+    marginLeft: 20,
+  },
+  ratingContainer: {
+    marginTop: 10,
+    flexDirection: 'row',
+  },
+  numReviewsText: {
+    marginLeft: 5,
+    marginTop: 5,
+  },
+});
--- types.ts
+++ types.ts
@@ -31,4 +31,25 @@ export type FoodItem = {
 
 export type MarkedFoodItem = FoodItem & { user_id: number; item_id: number };
 
-export type FoodItemDetail = {};
+export type FoodItemDetail = {
+  food_name: string;
+  brand_name: string;
+  serving_qty: number;
+  serving_unit: string;
+  serving_weight_grams: number;
+  nf_calories: number;
+  nf_total_fat: number;
+  nf_saturated_fat: number;
+  nf_cholesterol: number;
+  nf_sodium: number;
+  nf_total_carbohydrate: number;
+  nf_dietary_fiber: number;
+  nf_sugars: number;
+  nf_protein: number;
+  nf_potassium?: number;
+  nix_brand_id: string;
+  photo: {
+    thumb: 'string';
+  };
+  nix_item_id: string;
+};
